acbb1baa3664fef33a569c2c7efeb55d532fe56f
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-09-09 15:20:35 -0300

    Added file download

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
-import { Server, Socket } from 'socket.io';
+import { Server } from 'socket.io';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 
@@ -11,24 +13,42 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://127.0.0.1:3000',
+    origin: '*',
     methods: ['GET', 'POST'],
+    credentials: false,
   },
 });
 
 app.use(express.json());
 
 interface Message {
-  user: string;
+  id: string;
+  userId: string;
   message: string;
   date: string;
 }
 
+interface IDeleteMessage {
+  id: string;
+  adminId: string;
+}
+
 const messages: Message[] = [];
+const deletedMessages: Message[] = [];
+
+app.get('/download', (req, res) => {
+  const options = {
+    root: path.join(__dirname + '/public/deleted_messages.txt'),
+  };
+
+  res.sendFile(options.root);
+});
 
 io.on('connection', (socket) => {
-  socket.on('joinChat', (newUser) => {
+  socket.on('joinChat', () => {
     socket.emit('previousMessages', messages);
+
+    io.to(socket.id).emit('sendId', socket.id);
   });
 
   socket.on('sendMessage', (newMessage) => {
@@ -37,6 +57,29 @@ io.on('connection', (socket) => {
     socket.emit('previousMessages', messages);
 
     socket.broadcast.emit('receivedMessage', messages);
+  });
+
+  socket.on('deleteMessage', (data: IDeleteMessage) => {
+    const messageToDelete = messages.find((message) => message.id === data.id);
+
+    if (messageToDelete) {
+      const messageIndex = messages.indexOf(messageToDelete);
+
+      messages.splice(messageIndex, 1);
+
+      deletedMessages.push(messageToDelete);
+
+      const deletedMessagesText = `Id do remetente: ${messageToDelete.userId}, Id da mensagem: ${messageToDelete.id}, Menssagem: ${messageToDelete.message}"\n\n`;
+
+      fs.appendFileSync('src/public/deleted_messages.txt', deletedMessagesText);
+
+      io.to(data.adminId).emit('messageDeleted');
+
+      socket.emit('receivedMessage', messages);
+      socket.broadcast.emit('receivedMessage', messages);
+    } else {
+      throw new Error('Message not found');
+    }
   });
 
   socket.on('disconnect', () => {
