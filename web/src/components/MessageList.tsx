412f6354b0470e5f143f320e75bdadb643460293
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-09-09 15:21:36 -0300

    Updated messageList

--- web/src/components/MessageList.tsx
+++ web/src/components/MessageList.tsx
@@ -1,24 +1,53 @@
 import styles from './MessageList.module.css';
+import { Trash } from 'phosphor-react'
+import { FormEvent } from 'react';
 
 interface Message {
-    user: string,
-    message: string,
-    date: string
+    id: string;
+    userId: string;
+    user: string;
+    message: string;
+    date: string;
 }
 
 interface Messages {
-    messages: Message[]
+    isAdmin: boolean;
+    messages: Message[];
+    onDeleteMessage: (id: string) => void;
+    handleDownload:() => void;
 }
 
-export function MessageList(messages: Messages) {
+export function MessageList({messages, isAdmin, onDeleteMessage, handleDownload}: Messages) {
+
+    function deleteMessage (event: FormEvent, id: string) {
+        event.preventDefault()
+        onDeleteMessage(id);
+        handleDownload();
+    }
+    
     return(
         <div className={styles.messagesBox}>
-            {messages.messages.map((message) => {
+            {messages.map((message) => {
                 return (
-                    <div className={styles.message}>
-                        <div>
-                            <strong>{message.user}:</strong>
-                            <span>{message.message}</span>
+                    isAdmin === true ?
+                        <div key={message.id} className={styles.message}>
+                            <div className={styles.userMessage}>
+                                <strong>{message.user}: </strong>
+                                <span className={styles.messageText}>{message.message}</span>
+                            </div>
+                            <div className={styles.messageUtil}>
+                                <span>{message.date}</span>
+                                <button className={styles.trash} onClick={(event) => deleteMessage( event, message.id)} >
+                                    <Trash size={20} />
+                                </button>
+                            </div>
+                        </div>
+                    :
+
+                    <div key={message.id} className={styles.message}>
+                        <div className={styles.userMessage}>
+                            <strong>{message.user}: </strong>
+                            <span className={styles.messageText}>{message.message}</span>
                         </div>
                         <span>{message.date}</span>
                     </div>
