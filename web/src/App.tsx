43b2f023f747960a77046e67bb1bcae2d4e8c804
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-09-12 13:54:34 -0300

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,16 @@
-import { Chat } from './components/Chat'
-import './global.css'
-import styles from './App.module.css'
+import { WebChat } from "./components/WebChat/WebChat";
+import "./global.css";
+import styles from "./App.module.css";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-
   return (
     <div className={styles.container}>
       <div className={styles.box}>
-        <Chat />
+        <WebChat />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
